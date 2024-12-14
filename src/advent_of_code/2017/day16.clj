012944d62c2b35cd1fcf825b38ad806a19515d4f
Kevin Hermann <hermannkevin@gmail.com>
2024-12-14 04:36:22 +0100

    2017 day16

--- src/advent_of_code/2017/day16.clj
+++ src/advent_of_code/2017/day16.clj
@@ -16,11 +16,19 @@
   (let [[c & more] instruction
         s (str/join more)]
     (case c
-      \s (rotate v (Integer/parseInt s))
-      \x (apply swap v (map read-string (str/split s #"/")))
+      \s (u/rotate v (Integer/parseInt s))
+      \x (apply u/swap v (map read-string (str/split s #"/")))
       \p (apply swap-idx v (str/split s #"/"))
       )))
 
 (defn part-1 []
   (->> (reduce process start input)
        (str/join)))
+
+;; loop size = 560000
+(defn part-2 []
+  (loop [programs start [x & more] (cycle input) i 0]
+    (let [s (str/join programs)]
+      (when (= i 390353) (println s))
+      (if (= i 2000000) nil
+          (recur (process programs x) more (inc i))))))
